2b70cabd0423dc4c334d46fea8eddecd2bb77c7d
Mike Slinn <mslinn@micronauticsresearch.com>
2022-03-02 10:31:18 -0500

    -

--- src/extension.ts
+++ src/extension.ts
@@ -5,18 +5,18 @@ import * as vscode from 'vscode';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
+
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "redirect-generator" is now active!');
+	console.log('Extension "redirect-generator" is active');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('redirect-generator.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('redirect-generator.redirectHttp', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from redirect_generator!');
+		vscode.window.showInformationMessage('Add a redirect from redirect_generator');
 	});
 
 	context.subscriptions.push(disposable);
