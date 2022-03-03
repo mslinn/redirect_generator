21824f5499dfded8e658b545084c9e6178006ba2
Mike Slinn <mslinn@micronauticsresearch.com>
2022-03-03 14:20:41 -0500

    Making modules

--- src/jekyllConfig.ts
+++ src/jekyllConfig.ts
@@ -4,19 +4,30 @@ export class JekyllConfig {
   public hasCollectionsDir: boolean;
   public collectionsDir: string;
 
-  private documentWorkspaceFolder: any;
+  private hasDocumentWorkspaceFolder: boolean;
+  private documentWorkspaceFolder: string;
 
-  constructor(documentWorkspaceFolder: any) {
-    this.documentWorkspaceFolder = documentWorkspaceFolder;
+  constructor(documentWorkspaceFolder: string | undefined) {
+    this.hasCollectionsDir = false;
+    if (documentWorkspaceFolder) {
+      this.hasDocumentWorkspaceFolder = true;
+      this.documentWorkspaceFolder = documentWorkspaceFolder;
+    } else {
+      this.hasDocumentWorkspaceFolder = false;
+      this.documentWorkspaceFolder = '.';
+      console.log("Caution: this VSCode extension may not work unless a workspace is defined.");
+    }
     this.collectionsDir = '';
+    this.loadYaml();
+  }
 
+  private loadYaml() {
     const yaml = require('js-yaml');
     const fs   = require('fs');
-
     try {
-      const fqConfigName = this.documentWorkspaceFolder() + '/_config.yml';
+      const fqConfigName = this.documentWorkspaceFolder + '/_config.yml';
       const doc = yaml.load(fs.readFileSync(fqConfigName, 'utf8'));
-      console.log(doc);
+      // console.log(doc);
       this.hasCollectionsDir = doc.hasOwnProperty('collections_dir');
       if (this.hasCollectionsDir) {
         console.log("TODO: Figure out how to construct filename for collections_dir");
@@ -24,7 +35,6 @@ export class JekyllConfig {
       } else {
         console.log("TODO: Figure out how to construct filename without collections_dir");
       }
-      return this;
     } catch (e) {
       console.log(e);
       exit(1);
